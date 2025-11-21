06fdfd68eadef6d3ed0721026d07b5396d10c872
hanna <channashosh@gmail.com>
2025-11-21 16:35:48 +0200

    overseas backend

--- src/variables/services.js
+++ src/variables/services.js
@@ -1,5 +1,5 @@
-export const account = "http://localhost/api/account";
-export const connector = "http://localhost/api/connection";
-export const projectEndpoint = "http://localhost/api/project";
-export const serviceEndpoint = "http://localhost/api/service";
-export const auth = "http://localhost/api/auth";
+export const account = "http://overseas/api/account";
+export const connector = "http://overseas/api/connection";
+export const projectEndpoint = "http://overseas/api/project";
+export const serviceEndpoint = "http://overseas/api/service";
+export const auth = "http://overseas/api/auth";
