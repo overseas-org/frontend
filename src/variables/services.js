c169a03c5c0bb1a3e953c6a516258569d723fa2a
hanna <channashosh@gmail.com>
2025-11-21 17:57:02 +0200

    .com

--- src/variables/services.js
+++ src/variables/services.js
@@ -1,5 +1,5 @@
-export const account = "http://overseas/api/account";
-export const connector = "http://overseas/api/connection";
-export const projectEndpoint = "http://overseas/api/project";
-export const serviceEndpoint = "http://overseas/api/service";
-export const auth = "http://overseas/api/auth";
+export const account = "http://overseas.com/api/account";
+export const connector = "http://overseas.com/api/connection";
+export const projectEndpoint = "http://overseas.com/api/project";
+export const serviceEndpoint = "http://overseas.com/api/service";
+export const auth = "http://overseas.com/api/auth";
